b372c584c49afcbf5875ec8ccffd2092598cf035
Pratik Domadiya <pratikdomadiya123@gmail.com>
2023-07-21 18:38:20 -0400

    Update order_limit.sql

--- sample_dbt_project-master/models/order_limit.sql
+++ sample_dbt_project-master/models/order_limit.sql
@@ -4,6 +4,8 @@ Custom generic tests:
 Test : order limit not exceed to 500
 NOTE : To add this test into production , add configuration in schema.yaml file.
 
+let’s say the business requires that we have no orders for over 500 units, as the company cannot fulfill such large orders with current inventory levels. This is the perfect use case for customer generic tests.
+
 */
 
 
